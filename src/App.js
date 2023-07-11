9923f1a31be1145149ad28df2292b599a631eb5c
Bijay Sharma <bijaysharma8335@gmail.com>
2023-07-11 19:59:28 +0530

    File upload
    -Install dependencies
    -Upload excel file UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [data, setData] = useState([]);
+
+    //handler function
+    const onChangeHandler = (e) => {
+        const reader = new FileReader();
+        reader.readAsBinaryString(e.target.files[0]);
+        reader.onload = (e) => {
+            const data = e.target.result;
+            const workbook = XLSX.read(data, { type: "binary" });
+            const sheetName = workbook.SheetNames[0];
+            const sheet = workbook.Sheets[sheetName];
+            const parsedData = XLSX.utils.sheet_to_json(sheet);
+            setData(parsedData);
+        };
+    };
+    return (
+        <div className="App">
+            <input type="file" accept=".xlx,.xls" onChange={onChangeHandler} />
+        </div>
+    );
+};
 
 export default App;
